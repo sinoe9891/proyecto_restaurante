0cfbb951e5281ced80210c0fa6cbb37c0ff83b57
sinoe9891 <sinoeproducciones@gmail.com>
2022-06-21 01:51:49 -0600

    first commit

--- includes/templates/menu.php
+++ includes/templates/menu.php
@@ -3,7 +3,7 @@
         <nav class="navbar navbar-expand-lg custom_nav-container ">
           <a class="navbar-brand" href="index.html">
             <span>
-              Feane
+			  <img src="images/logo.png" alt="">
             </span>
           </a>
 
